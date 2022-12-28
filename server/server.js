bff52ab061b24879b674544856f3250bb125a0cb
adhilroshan <adhilchra@gmail.com>
2022-12-28 09:39:58 +0530

    The show is on

--- server/server.js
+++ server/server.js
@@ -43,4 +43,6 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
+app.listen(5000, () =>
+  console.log("Server is running on https://mikki.onrender.com")
+);
